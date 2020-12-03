edb444b21da6b4c3de5a8056a40e98e8285190ce
YaoKundi <kundiyao@gmail.com>
2020-12-03 10:15:12 -0500

    update chunk selection

--- py/main.py
+++ py/main.py
@@ -1570,7 +1570,8 @@ if __name__ == '__main__':
     input_dir = '../logs'
     repeat = 100
 
-    chunkSize = ['16K', '32K', '64K', '128K']
+    #chunkSize = ['16K', '32K', '64K', '128K']
+    chunkSize = ['256K', '512K', '1M', '2M', '4M', '8M', '16M', '32M']
 
     # If disable, this will block step1 to step4 one by one
     # Only works for evaluating LogBlock
